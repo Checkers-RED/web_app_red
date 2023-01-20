f2d04bb03f95fbc667de2117cb64197f697672be
Victoria <vbalzhieva@gmail.com>
2023-01-20 23:46:46 +0300

    адаптив авторизации

--- src/views/ForgotPass.vue
+++ src/views/ForgotPass.vue
@@ -141,8 +141,20 @@
 </script>
 
 <style lang="scss" scoped>  
+@media screen and (min-width: 1000px) {
   .container {
     width: 500px;
+  }
+  .mobile {
+    display: none;
+  }
+}
+@media screen and (max-width: 1000px) {
+  .container {
+    width: 95%;
+  }
+}
+  .container {
     background: #FFFFFF;
     border: 1px solid #E0E0E0;
     box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.25);
